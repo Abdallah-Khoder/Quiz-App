9aaecc30b55dd5a627f1f746a8610567602040e9
Abdallah-Khoder <abdallahkodor746@gmail.com>
2025-04-20 12:45:11 +0300

    check found user

---
+++ login.js
@@ -0,0 +1,30 @@
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    // When we submit the form, the page is reloaded. In order to save the data, I will prevent reloading.
+    e.preventDefault();
+    const usernameOrEmail = document.querySelector('input[name="usernameOrEmail"]').value;
+    const pass = document.querySelector('input[name="pass"]').value;
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    let foundUser = null;
+    for (let i = 0; i < users.length; i++) {
+        if ((users[i].email == usernameOrEmail || users[i].username == usernameOrEmail) && users[i].pass == pass ) {
+                foundUser = users[i];
+                break;
+        }
+    }
+
+    if(foundUser){
+        if(foundUser.email == "admin@quiz" && fonundUser.pass == "admin123"){
+            window.location.href = "dashboard.html";
+        }
+        else{
+            localStorage.setItem("loggedInUser", JSON.stringify(foundUser));
+            window.location.href = "home.html";
+        }
+    }
+    else{
+        alert("Users Not Found , Please sign in");
+        window.location.href = "register.html" ; 
+    }
+
+});
